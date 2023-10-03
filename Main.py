5d28dae48754bfb27b20f46dfb5b7c6167ca45aa
RJPElfrink <104073541+RJPElfrink@users.noreply.github.com>
2023-10-03 16:57:56 +0200

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,31 @@
+import numpy as np
+import scipy as sc
+from scipy import signal
+import matplotlib.pyplot as plt
+import rheosys as rhs
+
+
+
+#sampling time settings
+wave_duration = 3
+sample_rate = 200
+freq = 3
+q_factor = 5
+samples = wave_duration*sample_rate
+samples_decimated = int(samples/q_factor)
+
+#Frequency input selection from rheosys
+x_exact = np.linspace(0, wave_duration, samples, endpoint=False)
+y_exact = rhs.sin(x_exact*np.pi*freq*2)
+
+
+#Sampling frequency to time vector use of scipy DECIMATE
+x_vect_time = np.linspace(0, wave_duration, samples_decimated, endpoint=False)
+y_vect_time = signal.decimate(y_exact,q_factor,)
+
+
+#Figure plot of the corresponding fr
+plt.plot(x_exact, y_exact, '.-', x_vect_time, y_vect_time, 'o-')
+plt.xlabel('Time, Seconds')
+plt.legend(['data', 'decimated'], loc='best')
+plt.show
