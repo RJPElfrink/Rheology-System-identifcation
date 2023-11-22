ba062a94e909c82c68058e8e838726b3100b1334
Rutger Elfrink <rutgerelfrink@gmail.com>
2023-11-22 15:00:27 +0100

    commit

--- Main.py
+++ Main.py
@@ -14,9 +14,9 @@ def sine (period):
 # AVOID leakage: f_s/f_0 must be an integer
 
 #define excitation varibales
-f_s = 100                 # Sample frequency
-N= 100                    # Number of points
-f_0 = 20                 # Excitation frequency
+f_s = 10                 # Sample frequency
+N= 10                    # Number of points
+f_0 = 2                 # Excitation frequency
 
 phi= np.pi/3            # signal phase
 Up=100                  # Upsampling for plots
@@ -24,8 +24,8 @@ Up=100                  # Upsampling for plots
 # Calculation of time window
 T=N/f_s                 # Time length
 T_s = 1/f_s             # Sampling time
-t_DT= np.arange(0, T,1/f_s)        # Discrete-time vector
-t_CT= np.arange(0, T, 1/(f_s*Up))  # Continuous-time vector
+t_DT= np.linspace(0, T,N,endpoint=False)        # Discrete-time vector
+t_CT= np.linspace(0, t_DT[-1], N*Up,endpoint=True)  # Continuous-time vector
 
 
 u_DT=sine(2*np.pi*f_0*t_DT+phi)
@@ -54,6 +54,8 @@ fig0.supylabel('$|U_{DFT}|$')
 ax0.legend()
 plt.show()
 
+
+
 # Calculation of u_t, by use of scipy interpolation, kind is 0,2 and all odd numbers
 u_interp=interp1d(t_DT,u_DT,kind=0)
 u_t_interp = u_interp(t_CT)
@@ -72,7 +74,7 @@ def ODE_maxwell(t, tau, L,G):
     #L,G=args
     return G*gamma_dot(t) - tau/L
 
-sol = solve_ivp(ODE_maxwell, [0, T], [0], args=(1.5, 2.5), t_eval=t_DT)
+sol = solve_ivp(ODE_maxwell, [0, t_DT[-1]], [0], args=(1.5, 2.5), t_eval=t_DT)
 
 plt.plot(np.squeeze(t_DT), np.squeeze(sol.y))
 plt.xlabel('t')
--- Rheosys.py
+++ Rheosys.py
@@ -5,57 +5,10 @@ import scipy as sc
 #Excitation signals used as input for the analysis,
 #   input must be defined as one value with the unit of ...
 #   output is defined with the unit of ...
+def sine (period):
+    return np.sin(period)
 
-def sin (time):
-    ouput=np.sin(time)
-    return ouput
-
-def cos (x):
-    ouput=np.cos(x)
-    return ouput
-
-
-
-
-import math
-import sys
-
-def find_lcm(f_0, end_range):
-    lcm = f_0
-    for i in range(2, end_range + 1):
-        lcm = math.lcm(lcm, f_0 * i)
-    return lcm
-
-def is_divisible(N, f_0, i):
-    # Check if N divided by (f_0 * i) has no remainder
-    return N % (f_0 * i) == 0
-
-def window(f_0, N, end_range):
-
-    recommended_N_larger = N
-    recommended_N_smaller = N
-
-
-    # Search for the closest suitable N in both directions
-    while not (any(is_divisible(recommended_N_larger, f_0, i) for i in range(1, end_range + 1)) or any(is_divisible(recommended_N_smaller, f_0, i) for i in range(1, end_range + 1))):
-        recommended_N_larger += 1
-        recommended_N_smaller -= 1
-
-    # Choose the closest suitable N
-    if any(is_divisible(recommended_N_larger, f_0, i) for i in range(1, end_range + 1)) and any(is_divisible(recommended_N_smaller, f_0, i) for i in range(1, end_range + 1)):
-        recommended_N = recommended_N_larger if abs(N - recommended_N_larger) < abs(N - recommended_N_smaller) else recommended_N_smaller
-    elif any(is_divisible(recommended_N_larger, f_0, i) for i in range(1, end_range + 1)):
-        recommended_N = recommended_N_larger
-    else:
-        recommended_N = recommended_N_smaller
-
-
-
-    # Exit the program if N is not divisible by f_0
-    if not any(is_divisible(N, f_0, i) for i in range(1, end_range + 1)):
-        x=f_0*recommended_N
-        print(f"Selected N is not divisible by f_0.")
-        print(f"The recommended N for divisibility by ({f_0} * i) for i in the range 1 to {end_range} around {N} is {recommended_N}.")
-        print(f"To avoid leakage the sampling frequency is advised to be {x} [Hz]")
+def cos (period):
+    return np.cos(period)
 
 
