a3f54b336b9961967197d3b7e39abaf229a9281f
Rutger Elfrink <rutgerelfrink@gmail.com>
2024-01-31 11:50:14 +0100

    Code cleanup

--- Depricated-code/ODE_solve_ivp.py
+++ Depricated-code/ODE_solve_ivp.py
@@ -7,33 +7,29 @@ from scipy import signal
 import Rheosys as rhs
 
 
-"""Set transfer function parameters"""
-Lambda_constant=1.5                             # Value for lambda n/g
-g_constant=2.5                                  # Value for spring constant g
+# Script for calculating the FRF of the Maxwell model
+# CHANGE VALUES ONLY IN LINES 13-24
+
 
 f_s = 20                                        # Sample frequency
 N = 2000                                        # Number of points
 P = 4                                           # Number of repeated periods
-interpolation_kind=[True,0]                    # interpolation
-normalize_value   =[True,'STDev']              # Select normalization methode (None, STDev, Amplitude, RMS)
 
 J1=1                                            # Starting frequency multisine
-J2=N/4                                          # Stopping freqency multisine
-phase_selection = 'Schroeder'                   # Select phase for the multisine, (Schroeder,Random,Linear,Zero,Rudin,Newman)
+J2=N/2                                          # Stopping freqency multisine
 
-k_1 = 1                                         # Starting frequency chirp
-k_2 = N/4                                       # Stopping frequency chirp
+Lambda_constant=1.5                             # Value for lambda n/g
+g_constant=2.5                                  # Value for spring constant g
+kind=0                                          # interpolation kind
 
-plot_signal='Multisine'                         # Select 'Chirp' or 'Multsine' to change plots
-
-""" Calculation of Time window"""
-
+# Calculation of time window
 f_0 =f_s/N                                      # Excitation frequency
 T = N/f_s                                       # Time length
 t= np.linspace(0, T,N,endpoint=False)           # Time vector
 f=np.linspace(0,f_s,N,endpoint=False)           # Frequency range
 t_transient= np.linspace(0, T*P,N*P,endpoint=False)        # Transient-time vector
-
+t_steady = np.linspace(0,T,N,endpoint=False)
+A_strain = np.ones_like(f)                           # Strain rate per frequency
 
 ### Transfer function of the maxwell system ###
 # G=g/((1j*f)+1/L)
@@ -46,40 +42,41 @@ phase_tf= np.deg2rad(phase)             # Fase of transfer function in radians
 mag_tf=10**(mag/20)                     # Magnitude of transfer function [-]
 G_tf=mag_tf*np.exp(1j*(phase_tf))
 
-"""
-TRANSIENT STATE CALCULATIONS
-Calculations of the extended signal to remove the transient part
-and repeat the signal to the full time window of N*P
-clip the last period of Ntotal to select the steady state periode N
-"""
+### TRANSIENT STATE CALCULATIONS ###
+# Calculations of the extended signal to remove the transient part
+# and repeat the signal to the full time window of N*P
+# clip the last period of Ntotal to select the steady state periode N
 
-# Calculation of the input signal for the multisine signal over the entire transient time period
-u_mutlisine, phase_multisine=rhs.multisine(N,f_s, f_0, [J1,J2],P=P,phase_response=phase_selection,time_domain=True)
-
-# Calculation of the input signal for the multisine signal over the entire transient time period
-u_chirp=rhs.chirp_exponential(t,[k_1,k_2],f_s,N,P)
-
-#Select u_chirp or u_multi as tranient signal for figure generation
-if plot_signal=='Multisine':
-    u_transient=u_mutlisine
-elif plot_signal=='Chirp':
-    u_transient=u_chirp
-
-# Normalize the input signal to None, STDev, Amplitude or RMS
-if normalize_value[0]==True:
-    u_transient=rhs.normalization(u_transient,normalize=str(normalize_value[1]))
-
+# Calculation of the input signal over the entire transient time period
+u_transient_1, phase_u=rhs.multisine(N,f_s, f_0, [J1,J2],P=P,phase_response='Schroeder',time_domain=True)
+u_transient=u_transient_1
 # Sampled transient signal as a function of t
-if interpolation_kind[0]==True:
-    u_transient_t=interp1d(t_transient,u_transient,kind=interpolation_kind[1])
-    u_transient=u_transient_t(t_transient)
+u_transient_int_t=interp1d(t_transient,u_transient,kind=kind)
 
+# Calculation of the input signal over the entire transient time period as a function of t
+u_transient_t, phase_u=rhs.multisine(N,f_s, f_0, [J1,J2],P=P,phase_response='Schroeder',time_domain=False)
+u_transient=u_transient_t(t_transient)
+
+
+# Strain rate function, input signal dependent on t
+def gamma_dot(t):
+    return u_transient_t(t)
+
+# Maxwell function
+# tau_dot=G*gamma_dot(t) -1/lambda *tau
+def ODE_maxwell(t, tau, L,g):
+    return g*gamma_dot(t) - tau/L
+
+# Solve function for the Maxwel ODE
+sol_transient = solve_ivp(ODE_maxwell, [0, t_transient[-1]], [0], args=(Lambda_constant, g_constant), t_eval=t_transient)
 
 # Compute the response
-t_out,y_out, _ = signal.lsim(s1, U=u_transient, T=t_transient)
+#t_out,y_out, _ = signal.lsim(s1, U=u_transient, T=t_transient)
+
 
 # Solutions of the ODE
-y_transient=np.squeeze(y_out)                     # y output in time domain entire transient signal
+y_transient=np.squeeze(sol_transient.y)          # y output in time domain entire transient signal
+#y_transient=np.squeeze(y_out)                     # y output in time domain entire transient signal
 y_steady=y_transient[((N*P-N)):]                 # Copy of last steady period y output
 u_steady=u_transient[((N*P-N)):]                 # Copy of last steady period u input
 Y_steady=fft(y_steady)                           # Y output to frequency domain
@@ -87,8 +84,6 @@ U_steady=fft(u_steady)                           # U input to frequency domain o
 G_steady=Y_steady/U_steady                       # FRF of the multiphase
 mag_G=abs(G_steady)                              # Magnitude of G in time analysis in [-]
 phase_G=np.angle(G_steady)                       # Phase of G in time analysis in radians
-
-transient_analytic = np.exp(-t_transient/Lambda_constant) # analytical transient curve
 
 # Calculations of t he multiphase crest factor
 print('Crest factor: {0:.2f} dB'.format(rhs.DB(rhs.crest_fac(u_steady))))
@@ -131,11 +126,12 @@ plt.ylabel('Amplitude (dB)')
 plt.xlabel('Time (s)')
 plt.show()
 
+transient = np.exp(-t_transient/Lambda_constant) # set to zero to exclude transient
+
 # Figure of the created multisine signals frequency domain in Decibel
 plt.plot(t_transient,np.tile(y_steady,P)-y_transient,label='y-y$_p$')
-plt.plot(t_transient,transient_analytic,label='analytical transient')
+plt.plot(t_transient,transient,label='analytical transient')
 plt.ylabel('y-y$_p$')
-plt.ylim(-0.001,1.5*np.max(np.tile(y_steady,P)-y_transient))
 plt.legend()
 plt.xlabel('Time[s]')
 plt.title('Transient detection of the transient ouput y over steady output y')
@@ -144,6 +140,7 @@ plt.show()
 # Phase plot of the FRF transferfunction with multisine
 plt.plot(f,rhs.DB(mag_G),'-',label='Multisine response')
 plt.plot(w_tf,rhs.DB(mag_tf),'-',label="Transfer Function")
+#plt.plot(f,(abs(G_steady-G_tf)),'o',label='|G(j$\omega$)-G(j$\omega$)|')
 plt.plot(f,rhs.DB((abs(mag_G)-abs(mag_tf))),'o',label='|G(j$\omega$)-G(j$\omega$)|')
 plt.title('FRF magnitude response of the maxwell model G$_0$')
 plt.legend()
@@ -178,8 +175,8 @@ plt.ylabel('Magnitude [dB]')
 plt.subplot(2, 1, 2) # phase plot
 plt.plot(f*2*np.pi, np.rad2deg(phase_G), '-', label="Multisine response")
 plt.plot(w_tf*2*np.pi, np.rad2deg(phase_tf), '-', label="Transfer Function")
-plt.xlim([(f_s*2*np.pi)/N,f_s*np.pi])
 plt.legend()
+plt.xlim([(f_s*2*np.pi)/N,f_s*np.pi])
 plt.xscale('log')
 plt.ylabel('Phase')
 plt.xlabel('Frequency (rad/s)')
@@ -187,5 +184,3 @@ plt.xlabel('Frequency (rad/s)')
 # Show the figure
 plt.tight_layout()
 plt.show()
-
-
--- Rheosys.py
+++ Rheosys.py
@@ -1,231 +1,151 @@
 import numpy as np
-import scipy as sc
-import matplotlib.pyplot as plt
-from random import random
-from scipy.fft import fft
 
+# Functions for generating different types of signals
 
-#Excitation signals used as input for the analysis,
-#   input must be defined as one value with the unit of ...
-#   output is defined with the unit of ...
-
-
-
-def chirp_exponential(time,frequencie_limits,f_s,N,P):
-
-    f_0  = f_s/N
-    T = N/f_s                                        # Time length
-
-    k_1  = frequencie_limits[0]
-    k_2  = frequencie_limits[1]
-
-    L    = T/np.log(k_2/k_1)
-    signal= np.zeros_like(time)
-
-    for ii,t in enumerate(time):
-        signal[ii]=np.sin(2*np.pi*f_0*k_1*L*(np.exp((t/L))-1))
-
-    signal=np.tile(signal,P)
-
+def chirp_exponential(time, frequency_limits, f_s, N, P):
+    """
+    Generates an exponential chirp signal.
+    Parameters:
+    - time: Time vector.
+    - frequency_limits: Tuple of start and end frequencies.
+    - f_s: Sampling frequency.
+    - N: Number of samples.
+    - P: Number of periods.
+    """
+    f_0         = f_s / N
+    T           = N / f_s
+    k_1, k_2    = frequency_limits
+    L           = T / np.log(k_2 / k_1)
+    signal      = np.sin(2 * np.pi * f_0 * k_1 * L * (np.exp(time / L) - 1))
+    signal      = np.tile(signal, P)
     return signal
 
+# Functions for normalization of signals
 
-def fft_normalization(signal, ouput, **kwargs):
-    input=fft(signal)
-    output=fft(ouput)
-
-    normalize = kwargs.get('normalize', 'None')                 # Default normalization is None
-    # Normalize the signal spectrum
-    if isinstance(normalize, str):
-
-        if normalize == 'Amplitude':        #Amplitude normalization between -1 & 1
-            U = normalize_amp(input)
-            Y = normalize_amp(output)
-            print('check amp')
-        elif normalize == 'RMS':
-            U = normalize_rms(input)
-            Y = normalize_rms(output)
-        elif normalize == 'STDev':
-            U = normalize_stdev(input)
-            Y = normalize_stdev(output)
-        elif normalize == 'None':           # No normalization, output of pure amplification
-            print('check none')
-            U=input
-            Y=output
-            G=Y/U
-            return U,Y,G
-    else:
-            raise ValueError('Normalize must be Amplitude, RMS, STDev or None')
-    G=Y/U
-    print('check done')
-    return U, Y, G
-
-def DB(arr):
-    return 20 * np.log10(np.abs(arr))
-
-# Return the root mean square of all the elements in the array, flattened out
-def rms(arr):
-    rms = np.sqrt(np.mean(np.square(abs(arr))))
-    return rms
-
-#Calculation of the crest factor of the signal
-def crest_fac(frequency):
-    return (max(abs(frequency)))/rms(frequency)
-
-# Normalization of the signals amlitude, between -1 and 1
 def normalize_amp(signal):
-    signal = signal / np.max(np.abs(signal))
-    return signal
+    """
+    Normalizes the amplitude of the signal between -1 and 1.
+    """
+    return signal / np.max(np.abs(signal))
 
-# Normalization of the signals amlitude by RMS devision
 def normalize_rms(signal):
-    current_rms = rms(signal)
-    if current_rms > 0:
-        signal = signal / current_rms
-    return signal
+    """
+    Normalizes the signal by its root mean square value.
+    """
+    rms_val = np.sqrt(np.mean(np.square(abs(signal))))
+    return signal / rms_val if rms_val > 0 else signal
 
-# Normalization of the signals amplitude by standard deviation devision
 def normalize_stdev(signal):
-    current_stdev = np.std(signal)
-    if current_stdev > 0:
-        signal = signal / current_stdev
-    return signal
+    """
+    Normalizes the signal by its standard deviation.
+    """
+    std_val = np.std(signal)
+    return signal / std_val if std_val > 0 else signal
 
-# Multisine phase generation of phi=-j(j-1)*pi/F
-# Where F is the total amount of frequencies crest of 1.89
-def schroeder_phase(j_range,J):
-    phase=np.zeros(J)
-    for i in range(len(j_range)):
-        phase[i]= -1*(j_range[i])*(j_range[i]-1)*np.pi/J
-    return phase
-
-# Multisine linear phase generation crest of 28.0
-def linear_phase(tau,j_range,f_s,N):
-    phase=np.zeros(len(j_range))
-    #phi=-Tau*2*pi*f_s*(j/N)
-    for i in range(len(j_range)):
-        phase[i]=-j_range[i]*tau*2*np.pi*f_s/N
-    return phase
-
-# Phase generation which looks like an inverse schroeder with crest factor of 1.90
-def rudin_phase(J):
-
-    def rudinshapiro(J):
-        #Return first N terms of Rudin-Shapiro sequence.
-        def hamming(x):
-
-            return bin(x).count('1')
-
-        out = np.empty(J, dtype=int)
-        for n in range(J):
-            b = hamming(n << 1 & n)
-            a = (-1)**b
-            out[n] = a
-
-        return out
-
-
-    phase = -np.pi*rudinshapiro(J)
-    phase[phase == -np.pi] = 0
-
-    return phase
-
-#Sweep-like phase generation, crest of 2.65
-def newman_phase(J,j1):
-
-    k = np.arange(J) + j1
-    phase = (np.pi*(k-1)**2)/J
-    return phase
-
-# Generation of the multisine signal with input N,f_s, [j1,j2] and phase_response='schroeder'
-def multisine(N, f_s,f_0, frequency_limits, P=1 , A_vect=None, Tau=1, **kwargs):
-
-    j1=int(np.floor(frequency_limits[0]))                 # Starting frequency multisine
-    j2=int(np.ceil(frequency_limits[1]))                  # Ending frequency multisine
-    J=int(j2-j1)                                     # Total frequencies multisine
-    j_range=np.ndarray.astype(np.linspace(j1,j2-1,J,endpoint=True),int)             # Range of j1 to j2
-
-    #Kwargs defenitions
-    phase_response = kwargs.get('phase_response', 'Random')     # Default phase is Random
-    normalize = kwargs.get('normalize', 'None')                 # Default normalization is None
-    time_domain = kwargs.get('time_domain', True)               # Default output without t function
-
-
-    # If args is empty, set A_vect to np.ones(N), A is an amplification vector for the multisine
-    if A_vect is None:
-        A = np.ones(N)
-    elif np.size(A_vect) < J:
-        raise ValueError(f'The size of A must be at least equal to {J} (j2-j1)')
+def normalization(signal, normalize='None'):
+    """
+    Normalizes the signal based on the specified method.
+    """
+    if normalize == 'Amplitude':
+        return normalize_amp(signal)
+    elif normalize == 'RMS':
+        return normalize_rms(signal)
+    elif normalize == 'STDev':
+        return normalize_stdev(signal)
+    elif normalize == 'None':
+        return signal
     else:
-        A = A_vect[:J]
+        raise ValueError('Normalize must be "Amplitude", "RMS", "STDev", or "None"')
 
-    # If args is empty, set A_vect to np.ones(N), A is an amplification vector for the multisine
-    #if Tau is None:
-    #    tau = 1
-    #else:
-    #    tau = Tau
+# Functions for phase generation in multisine signals
 
+def schroeder_phase(j_range, J):
+    """
+    Generates Schroeder phase for multisine signals.
+    """
+    return np.array([-j * (j - 1) * np.pi / J for j in j_range])
+
+def linear_phase(tau, j_range, f_s, N):
+    """
+    Generates linear phase for multisine signals.
+    """
+    return np.array([-j * tau * 2 * np.pi * f_s / N for j in j_range])
+
+def rudin_phase(J):
+    """
+    Generates Rudin phase for multisine signals.
+    """
+    rudin_shapiro = lambda J: np.array([(-1) ** bin(n << 1 & n).count('1') for n in range(J)])
+    phase         = -np.pi * rudin_shapiro(J)
+    phase[phase == -np.pi] = 0
+    return phase
+
+def newman_phase(J, j1):
+    """
+    Generates Newman phase for multisine signals.
+    """
+    k     = np.arange(J) + j1
+    return (np.pi * (k - 1) ** 2) / J
+
+# Main function for generating multisine signals
+
+def multisine(N, f_s, f_0, frequency_limits, P=1, A_vect=None,phase_response='Schroeder', Tau=1 , time_domain=True):
+    """
+    Generates a multisine signal.
+    Parameters:
+    - N: Number of samples.
+    - f_s: Sampling frequency.
+    - f_0: Base frequency.
+    - frequency_limits: Tuple of start and end frequencies.
+    - P: Number of periods.
+    - A_vect: Amplitude vector.
+    - Tau: Time constant for linear phase generation.
+    - phase_response: Type of phase response ('Random', 'Schroeder', etc.).
+    - time_domain: Boolean indicating if output is a function of time u(t) with lambda t.
+    """
+
+    j1, j2        = map(int, (np.floor(frequency_limits[0]), np.ceil(frequency_limits[1])))
+    J             = j2 - j1
+    j_range       = np.linspace(j1, j2 - 1, J, endpoint=True).astype(int)
+
+    # Set up amplitude vector
+    A             = np.ones(N) if A_vect is None else A_vect[:J]
 
     # Select phase response
-    if isinstance(phase_response, str):
-            if phase_response == 'Schroeder':
-                phase = schroeder_phase(j_range,J)
-            elif phase_response == 'Random':
-                phase = np.random.uniform(0,2*np.pi,J)
-            elif phase_response == 'Linear':
-                phase = linear_phase(Tau,j_range,f_s,N)
-            elif phase_response == 'Newman':
-                phase = newman_phase(J,j1)
-            elif phase_response == 'Rudin':
-                phase = rudin_phase(J)
-            elif phase_response == 'Zero':
-                phase = np.zeros(N)
+    phase         = {
+        'Schroeder': schroeder_phase(j_range, J),
+        'Random'   : np.random.uniform(0, 2 * np.pi, J),
+        'Linear'   : linear_phase(Tau, j_range, f_s, N),
+        'Newman'   : newman_phase(J, j1),
+        'Rudin'    : rudin_phase(J),
+        'Zero'     : np.zeros(N)
+    }.get(phase_response, ValueError('Invalid phase response'))
 
-            else:
-                raise ValueError('Phase Response must be Random, Schroeder, Zero, Rudin, Newman or Linear.')
-    else:
-        phase = phase_response
-
-    # Calculation of the multisine with corresponding phase
-    # Output u is not a function but a signal array
+    # Calculate multisine signal
+    u = np.zeros(N * P) if time_domain else lambda k: sum(A[j] * np.cos(j_range[j] * k * 2 * np.pi * f_0 + phase[j]) for j in range(J))
     if time_domain:
+        T = (P * N) / f_s
+        t = np.linspace(0, T, N * P, endpoint=False)
+        for j in range(J):
+            u += A[j] * np.cos(j_range[j] * t * 2 * np.pi * f_0 + phase[j])
 
-        u = np.zeros(N*P)                                    # Signal vector
-        T = (P*N)/f_s                                        # Time length
-        t= np.linspace(0, T,N*P,endpoint=False)            # Time vector
+    return u, phase
+# Utility functions
 
-        for j in range(len(j_range)):
-            u+= A[j]*np.cos(j_range[j]*t*2*np.pi*f_0 + phase[j])
+def DB(signal):
+    """
+    Calculates the decibel value of the signal.
+    """
+    return 20 * np.log10(np.abs(signal))
 
-    # Output of u is a function dependend on t, u(t)
-    else:
-        #def u_of_t(x):
-           # signal = np.zeros_like(x)
-          #  for j in range(J):
-         #       signal += A[j] * np.sin(j_range[j] * x * 2 * np.pi * f_0 + phase[j])
-        #    return signal
+def rms(signal):
+    """
+    Calculates the root mean square of the signal.
+    """
+    return np.sqrt(np.mean(np.square(abs(signal))))
 
-        u = lambda k: sum(A[j] * np.cos(j_range[j] * k * 2 * np.pi * f_0 + phase[j]) for j in range(len(j_range)))
-        return u,phase
-
-    # Normalize the signal spectrum
-    if isinstance(normalize, str):
-
-        if normalize == 'Amplitude':        #Amplitude normalization between -1 & 1
-            u = normalize_amp(u)
-        elif normalize == 'RMS':
-            u = normalize_rms(u)
-        elif normalize == 'STDev':
-            u = normalize_stdev(u)
-        elif normalize == 'None':           # No normalization, output of pure amplification
-            return u,phase
-
-        #elif normalize == 'Power':
-            #normalize = rhs.normalize_power(u)
-    else:
-                raise ValueError('Normalize must be Amplitude, RMS, STDev or None')
-
-    return u,phase
-
-
-
+def crest_fac(signal):
+    """
+    Calculates the crest factor of the signal.
+    """
+    return max(abs(signal)) / rms(signal)
